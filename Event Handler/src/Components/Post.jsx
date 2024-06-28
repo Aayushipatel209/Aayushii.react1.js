eade5166d381f25b97f990ed6021d67e9bc951da
Aayushi patel <ayushipatel1661@gmail.com>
2024-06-28 22:58:48 +0530

    update-1

--- Event Handler/src/Components/Post.jsx
+++ Event Handler/src/Components/Post.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 const Post=({body,title})=>{
      return(
- <div style={{border:"2px solid #088395",padding:"5px",marginTop:"35px"}} >
-   
+ <div style={{border:"2px solid #088395",padding:"5px",marginTop:"35px",width:"950px"}} >
       <h1>{title}</h1>
     <h3>{body}</h3>
-          
  </div>
      )
 }
