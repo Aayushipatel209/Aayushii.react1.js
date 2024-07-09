97c4465f30c91ba3380938c8381384d2ae89b850
Aayushi patel <ayushipatel1661@gmail.com>
2024-07-09 09:52:56 +0530

    update-1

---
+++ Integrate Bootstrap template/README.md
@@ -0,0 +1,1 @@
+my url="https://drive.google.com/file/d/1lPLcc90vBKhOziNCWNrSPFfkW_DwiuJN/view?usp=sharing"
---
+++ Integrate Bootstrap template/src/App.jsx
@@ -0,0 +1,11 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Header from  './Header'
+function App() {
+  return (
+    <>
+    <Header/>
+    </>
+  )
+}
+export default App
